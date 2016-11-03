cae4c276ab7e613817c968865140c49337485d3b
Sebastian Brenner <Sebastian Brenner@DESKTOP-0EL0GTR>
2016-11-03 16:19:56 +0100

    Test

---
+++ JavaLabor/src/Labor1.java
@@ -0,0 +1,13 @@
+public class Labor1 {
+
+	public static void main(String[] args) {
+		
+		int myFirstVariable=42;
+		
+	
+		System.out.println("Los gehts");
+		System.out.println(myFirstVariable);
+
+	}
+
+}
