2960d40dff390191d6b1f8aaf9589a4a71927565
sebas <sebas@141.79.43.243>
2016-11-09 11:41:49 +0100

    True Fehler alter beheben

--- JavaLabor/src/Achterbahn.java
+++ JavaLabor/src/Achterbahn.java
@@ -2,15 +2,37 @@
 public class Achterbahn {
 
 	public static void main(String[] args) {
-
 		
+		Achterbahn achterbahn=new Achterbahn();
+		
+		achterbahn.alter=1;
+		achterbahn.Koerpergroese=145;
+		achterbahn.mitEltern=true;
+		achterbahn.nuechtern=true;
+		
+		
+		System.out.println("Berechtigt= " +achterbahn.einlasskontrolle());
+
 	}
 	
+	int alter;
+	int Koerpergroese;
+	boolean mitEltern;
+	boolean nuechtern;
+	
 	boolean einlasskontrolle(){
-		int alter;
-		int groese;
-		boolean nuechtern;
-		boolean eltern;
 		
-		if((alter<=16 || alter>12 && eltern) && (groese >= 145 && groese >= 205) && (nuechtern){
+		if(alter >= 16 || (alter <= 12 && mitEltern)){
+			if((Koerpergroese >= 145 && Koerpergroese <= 205) && (nuechtern)){
+			
+			return true;
+			
+		}
+		
+		}
+		
+	
+		return false;
+	}
+
 }
