668579e7e4171120a9ece8a1803654bfc5afdfe3
sebas <sebas@141.79.43.243>
2016-11-09 10:54:22 +0100

    Achterbahn Programm

--- JavaLabor/src/Achterbahn.java
+++ JavaLabor/src/Achterbahn.java
@@ -2,8 +2,15 @@
 public class Achterbahn {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
 	}
-
+	
+	boolean einlasskontrolle(){
+		int alter;
+		int groese;
+		boolean nuechtern;
+		boolean eltern;
+		
+		if((alter<=16 || alter>12 && eltern) && (groese >= 145 && groese >= 205) && (nuechtern){
 }
